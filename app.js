bca54f8eff4dd515d0f503a630253dd6c8f5631c
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-04 07:22:13 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,169 @@
+
+
+// var isValid=true;
+// console.log("your id card is Valid:", isValid);
+
+
+// isValid=false;
+// console.log("your id card is Valid:", isValid);
+
+
+
+
+
+// var userName="zeeshan Ahmed";
+// var number="123456789";
+// var price = 1222;
+// var isExective=true;
+
+
+// console.log(userName + " is Executive  officer: " , isExective);
+
+
+
+// var cities=["Karachi","Multan","Peshawar","Islamabad","Lahore"]
+
+
+// cities.replace("," , " ")
+// console.log(cities)
+
+
+
+
+
+// var cars = ["City" , "Civic" , "GLI" , "Toyota" , "Mehran" , "Marcedes"]
+
+// var r = cars.join(" ")
+// console.log(r);
+// document.write(r)
+
+// console.log(cars.indexOf(r))
+
+
+
+// var name="zeeshan Ahmed Is a Developer!"
+
+
+// var name1=name.replace(/z/,function (x){
+//     return x.toUpperCase();
+// })
+
+
+
+
+
+
+// var cities=["Karachi","Multan","Peshawar","Islamabad","Lahore","Quetta"]
+
+// var ind=cities.indexOf("Islamabad")
+// ind="Mansehra"
+
+// console.log(ind)
+// console.log ( " The capital of Pakistan is" , cities[ind]);
+
+
+
+
+
+
+
+
+
+// var obj={
+//     carName:"Civic",
+//     manufacturer:"Honda",
+//     isElectric:false,
+//     price: 350000
+// }
+
+// // console.log(obj)
+
+// for  (var key in obj){
+
+//     document.write(obj[key])
+
+// };
+
+
+
+
+
+
+
+
+// var student={
+//     name:"Zeeshan",
+//     age:18,
+//     student:true,
+//     batch:7,
+//     campus:"Gulshan",
+//     office:false
+// }
+
+
+// student.fatherName="Muhammad Mumtaz"
+// for(var key in student){
+//     document.write([key] , ":" ,student[key] + " <br>")
+// }
+
+
+var arry=["Zeeshan","Ahmed","Adnan","Ahmed"]
+
+
+console.log(arry)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
